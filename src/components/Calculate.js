4235128ecb84e75f215468f7d3dd8f679e971133
muskanvaswan <muskanvaswan@gmail.com>
2021-01-07 00:09:14 +0530

    making resistance input boxes draggable

--- src/components/Calculate.js
+++ src/components/Calculate.js
@@ -11,44 +11,50 @@ export default class Calculate extends React.Component {
 
 	render() {
 		return (
-      <div>
-        <div className="row">
-          <div className="col-md-8 row">
-            {this.state.resistances.map((resistance, i) =>
-              <div className="col-md-6">
-              <div key={i} className=" d-flex form-group input-group border rounded bg-light ">
-                <label htmlFor={i}  className="my-auto mx-1">R{i+1}</label>
-                <input name={i} value={resistance} onChange={this.handleChange} onDragOver={this.dropHandler} onDrop={this.handleDrop} className="form-control"></input>
-                <button data-index={i} onClick={this.deleteResistor} className="btn btn-danger">-</button>
-              </div>
-              </div>
-            )}
+      <div className="row container border p-3 rounded m-0">
+        <div className="col-md-10">
+          <div className="row">
+            <div className="col-md-8 row">
+              {this.state.resistances.map((resistance, i) =>
+                <div className="col-md-6">
+                  <div key={i} className=" d-flex form-group input-group border rounded bg-light ">
+                    <label htmlFor={i}  className="my-auto mx-1">R{i+1}</label>
+                    <input name={i} value={resistance} draggable onDragStart={this.dragStartResistor} onChange={this.handleChange} onDragOver={this.dropHandler} onDrop={this.handleDrop} className="form-control"></input>
+                    <button data-index={i} onClick={this.deleteResistor} className="btn btn-danger">-</button>
+                  </div>
+                </div>
+              )}
+            </div>
+            <div className="col-md-4">
+            <div className="form-group">
+              <select value={this.state.type} name="type" onChange={this.handleSelect} className="form-control">
+                <option value="s">Series</option>
+                <option value="p">parallel</option>
+              </select>
+            </div>
+            </div>
           </div>
-          <div className="col-md-4">
-          <div className="form-group">
-            <select value={this.state.type} name="type" onChange={this.handleSelect} className="form-control">
-              <option value="s">Series</option>
-              <option value="p">parallel</option>
-            </select>
-          </div>
-          </div>
-        </div>
 
-        <div className="row">
-          <div className="col-md-6">
-          <button onClick={this.addResistor} className="btn btn-primary w-100">+ Add Resistor</button>
-          </div>
-          <div className="col-md-6">
-            <button onClick={this.calculate} className="btn btn-success w-100"> ReCalculate</button>
+          <div className="row">
+            <div className="col-md-6">
+            <button onClick={this.addResistor} className="btn btn-primary w-100">+ Add Resistor</button>
+            </div>
+            <div className="col-md-6">
+              <button onClick={this.calculate} className="btn btn-success w-100"> ReCalculate</button>
+            </div>
           </div>
         </div>
-        <h2 draggable onDragStart={this.dragStart}>{this.state.answer}</h2>
+        <div draggable onDragStart={this.dragStartAnswer} className="col-md-2 border rounded p-4 d-flex bg-light overflow-auto"><h2   className="mx-auto">{this.state.answer}</h2></div>
       </div>
     );
 	}
-  dragStart = (e) => {
+  dragStartAnswer = (e) => {
     var ans = this.state.answer;
     e.dataTransfer.setData('val', ans)
+  }
+  dragStartResistor = (e) => {
+    var r = e.target.value;
+    e.dataTransfer.setData('val', r)
   }
   dropHandler = (e) =>{
     e.preventDefault();
@@ -97,7 +103,6 @@ export default class Calculate extends React.Component {
   }
 
   calculate = () =>{
-    console.log(this.state.type)
     if (this.state.type == "p"){
       this.setState(state => ({
         answer: parallel(state.resistances)
