5df3af9ffc2d1d688b7b746cb0eda5e40da4083a
muskanvaswan <muskanvaswan@gmail.com>
2021-01-07 00:09:38 +0530

    changing calculate to mutltiple

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,12 @@
 import React from "react";
 import './App.css';
 import Calculate from './Calculate';
+import Multiple from './Multiple';
+
 
 export default class App extends React.Component {
 	render() {
-		return ( < Calculate />
+		return ( < Multiple />
 		)
 	}
 }
