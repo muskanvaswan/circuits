daa37f37e8d5b34a9f77654ea1879c3fa005da2d
muskanvaswan <muskanvaswan@gmail.com>
2021-01-08 23:09:27 +0530

    made any input box to any resistor input box...

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ export default class App extends React.Component {
 		return (
 			<div>
 				<h4 className="text-white">Circuit Calculator</h4>
-				<p className="text-white">You can add another set of resistances with the add connection button. You can drag and drop your resistances from one connection to other and basically to any input box. Don't be scared if a "NaN" appears, it just means the answer is Not a Number and that you're going to die. Rest is self explanatory. You're welcome..</p>
+				<p className="text-white">You can add another set of resistances with the add connection button. You can drag and drop your resistances from one connection to other and basically to any resistor input box. Don't be scared if a "NaN" appears, it just means the answer is Not a Number and that you're going to die. Rest is self explanatory. You're welcome..</p>
 				< Multiple />
 			</div>
 		)
